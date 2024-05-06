6285155d5ae0fa34d45a388fb83f884d823103bc
Grayvox <144566632+Grayvox@users.noreply.github.com>
2024-05-06 18:43:49 -0400

    Add Game play method
    Adds the core play method for the Game itself.
    The actual gameplay loop will be in a seperate
    method.

--- lib/game.rb
+++ lib/game.rb
@@ -1,12 +1,33 @@
 # frozen_string_literal: true
 
+require_relative 'game_logic'
+require_relative 'text'
+require_relative 'character'
+
 # Defines the game itself
 class Game
+  include GameLogic
+  include Text
+  include Character
+
   private
 
   attr_accessor :player_name, :secret_word, :display_word, :incorrect_guesses, :already_guessed
 
   public
+
+  def play
+    puts 'First, enter your name.'
+    @player_name = gets.chomp
+    puts 'Great! The game can now start.'
+
+    @secret_word = pick_random_word
+    @display_word = generate_display(@secret_word)
+    @incorrect_guesses = 0
+    @already_guessed = []
+  end
+
+  private
 
   def guess_letter(start_text: true)
     puts 'Please input your next guess.' if start_text
