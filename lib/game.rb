10314510b6a91c19b7f96c9e130242abb157e236
Grayvox <144566632+Grayvox@users.noreply.github.com>
2024-05-06 16:58:56 -0400

    Rework Game class attributes
    Changes how the Game class attributes are defined,
    as there is no need for them to be read outside
    the class.

--- lib/game.rb
+++ lib/game.rb
@@ -2,9 +2,7 @@
 
 # Defines the game itself
 class Game
-  attr_reader :incorrect_guesses, :already_guessed, :secret_word, :display_word
-
   private
 
-  attr_writer :incorrect_guesses, :already_guessed, :secret_word, :display_word
+  attr_accessor :incorrect_guesses, :already_guessed, :secret_word, :display_word
 end
