85e06163851e08c4ddac2c5000daa27b1f59bccc
Grayvox <144566632+Grayvox@users.noreply.github.com>
2024-05-06 17:30:18 -0400

    Add guess comparison method
    Adds a method that compares a letter guessed to
    the secret word and the word "on display"

--- lib/game_logic.rb
+++ lib/game_logic.rb
@@ -8,4 +8,15 @@ module GameLogic
       return line if line.length < 12 && line.length > 5
     end
   end
+
+  def compare_guess(display, secret, guess)
+    display_arr = display.split(' ')
+    secret_arr = secret.split('')
+
+    secret_arr.each_with_index do |letter, index|
+      display_arr[index] = letter if secret_arr[index] == guess
+    end
+    puts 'Correct! You guessed successfully.'
+    display_arr.join(' ')
+  end
 end
