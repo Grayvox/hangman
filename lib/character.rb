0c786c1b07700ada2d6d9ae7746100033eb07b42
Grayvox <144566632+Grayvox@users.noreply.github.com>
2024-05-06 19:10:31 -0400

    Add word display to character possibilities
    Adds the current display word to be shown along
    with the character possibilities

--- lib/character.rb
+++ lib/character.rb
@@ -2,7 +2,7 @@
 
 # Contains all the possible drawings of the hanging man
 module Character
-  def no_wrong_guesses
+  def no_wrong_guesses(display)
     %(
         ________
         |      |
@@ -11,10 +11,12 @@ module Character
         |
 
     Incorrect Guesses: 0/6
+
+      #{display}
     )
   end
 
-  def one_wrong_guess
+  def one_wrong_guess(display)
     %(
         ________
         |      |
@@ -23,10 +25,12 @@ module Character
         |
 
     Incorrect Guesses: 1/6
+
+      #{display}
     )
   end
 
-  def two_wrong_guesses
+  def two_wrong_guesses(display)
     %(
         ________
         |      |
@@ -35,10 +39,12 @@ module Character
         |
 
     Incorrect Guesses: 2/6
+
+      #{display}
     )
   end
 
-  def three_wrong_guesses
+  def three_wrong_guesses(display)
     %(
         ________
         |      |
@@ -47,10 +53,12 @@ module Character
         |
 
     Incorrect Guesses: 3/6
+
+      #{display}
     )
   end
 
-  def four_wrong_guesses
+  def four_wrong_guesses(display)
     %(
         ________
         |      |
@@ -59,10 +67,12 @@ module Character
         |
 
     Incorrect Guesses: 4/6
+
+      #{display}
     )
   end
 
-  def five_wrong_guesses
+  def five_wrong_guesses(display)
     %(
         ________
         |      |
@@ -71,10 +81,12 @@ module Character
         |     /
 
     Incorrect Guesses: 5/6
+
+      #{display}
     )
   end
 
-  def dead_character
+  def dead_character(display)
     %(
         ________
         |      |
@@ -83,27 +95,29 @@ module Character
         |     / \\
 
     Incorrect Guesses: 6/6
+
+      #{display}
     )
   end
 
   # rubocop:disable Metrics/MethodLength
   # rubocop:disable Metrics/CyclomaticComplexity
-  def draw_character(wrong_guesses)
+  def draw_character(wrong_guesses, display)
     case wrong_guesses
     when 0
-      no_wrong_guesses
+      no_wrong_guesses(display)
     when 1
-      one_wrong_guess
+      one_wrong_guess(display)
     when 2
-      two_wrong_guesses
+      two_wrong_guesses(display)
     when 3
-      three_wrong_guesses
+      three_wrong_guesses(display)
     when 4
-      four_wrong_guesses
+      four_wrong_guesses(display)
     when 5
-      five_wrong_guesses
+      five_wrong_guesses(display)
     when 6
-      dead_character
+      dead_character(display)
     end
   end
   # rubocop:enable Metrics/MethodLength
