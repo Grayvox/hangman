b61a729e1f90b873349de2fc5cc4f8fdd5ef33e6
Grayvox <144566632+Grayvox@users.noreply.github.com>
2024-05-06 18:34:46 -0400

    Add letter guessing method
    Adds the method that takes an input of one letter
    as this turn's guess, and checks if the input is
    ONLY ONE letter and has not been guessed yet.

--- lib/game.rb
+++ lib/game.rb
@@ -5,4 +5,19 @@ class Game
   private
 
   attr_accessor :player_name, :secret_word, :display_word, :incorrect_guesses, :already_guessed
+
+  public
+
+  def guess_letter(start_text: true)
+    puts 'Please input your next guess.' if start_text
+    guess = gets.chomp.downcase
+    if guess.length != 1
+      puts 'Hey! Guesses must be only ONE letter. Please try again.'
+      return guess_letter(start_text: false)
+    elsif @already_guessed.include?(guess)
+      puts 'That letter has already been guessed. Please try again.'
+      return guess_letter(start_text: false)
+    end
+    guess
+  end
 end
