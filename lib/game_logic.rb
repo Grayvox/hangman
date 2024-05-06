760e5f8740c1513ff3d92d56b88a7e98a44e22d1
Grayvox <144566632+Grayvox@users.noreply.github.com>
2024-05-06 16:41:07 -0400

    Add criteria for random word method
    Refactors the random word method to only give
    words that are between 5 and 12 characters in
    length.

--- lib/game_logic.rb
+++ lib/game_logic.rb
@@ -3,6 +3,9 @@
 # Contains the main logic methods for the game
 module GameLogic
   def pick_random_word
-    File.readlines('./words.txt').sample
+    words = File.readlines('./words.txt').shuffle
+    words.each do |line|
+      return line if line.length < 12 && line.length > 5
+    end
   end
 end
